7b49c329c8d67ab7103ed370145ca5182227219f
hector <hector@hectordeMacBook-Pro.local>
2022-06-06 18:14:48 +0800

    feature: case 2

--- case.py
+++ case.py
@@ -12,26 +12,26 @@ from datetime import datetime
 
 def main():
     print(sys.path)
-    # engine = BinanceApiBacktestingEngine()
-    # engine.set_parameters(
-    #     vt_symbol="BTCUSDT.BINANCE",
-    #     interval="15m",
-    #     start=datetime.strptime("2022-03-01 00:00:00", "%Y-%m-%d %H:%M:%S"),
-    #     end=datetime.strptime("2022-04-30 00:00:00", "%Y-%m-%d %H:%M:%S"),
-    #     rate=float("0.001"),
-    #     slippage=float("0.001"),
-    #     size=1,
-    #     pricetick=float("0.001"),
-    #     capital=100000,
-    # )
-    #
-    # engine.add_strategy(DoubleMaStrategy, {})
-    #
-    # engine.load_data()
-    # engine.run_backtesting()
-    # daily_df = engine.calculate_result()
-    # statistics = engine.calculate_statistics()
-    # print(statistics)
+    engine = BinanceApiBacktestingEngine()
+    engine.set_parameters(
+        vt_symbol="BTCUSDT.BINANCE",
+        interval="15m",
+        start=datetime.strptime("2022-03-01 00:00:00", "%Y-%m-%d %H:%M:%S"),
+        end=datetime.strptime("2022-04-30 00:00:00", "%Y-%m-%d %H:%M:%S"),
+        rate=float("0.001"),
+        slippage=float("0.001"),
+        size=1,
+        pricetick=float("0.001"),
+        capital=100000,
+    )
+
+    engine.add_strategy(DoubleMaStrategy, {})
+
+    engine.load_data()
+    engine.run_backtesting()
+    daily_df = engine.calculate_result()
+    statistics = engine.calculate_statistics()
+    print(statistics)
 
 
 if __name__ == '__main__':
